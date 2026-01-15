585f44234adfd3074a71ed197d1fe085d59f5c1a
fengqiyu0317 <2668386407@qq.com>
2026-01-15 22:11:56 +0800

    Remove __pycache__ and workspace folders from src directory

--- src/workspace/rv32i_cpu/simulator/target/release/build/zerocopy-f120fe00d31eb5dc/build_script_build-f120fe00d31eb5dc.d
+++
@@ -1,5 +0,0 @@
-/mnt/d/Tomato_Fish/assassyn/src/workspace/rv32i_cpu/simulator/target/release/build/zerocopy-f120fe00d31eb5dc/build_script_build-f120fe00d31eb5dc: /tmp/cargo/registry/src/index.crates.io-6f17d22bba15001f/zerocopy-0.8.33/build.rs
-
-/mnt/d/Tomato_Fish/assassyn/src/workspace/rv32i_cpu/simulator/target/release/build/zerocopy-f120fe00d31eb5dc/build_script_build-f120fe00d31eb5dc.d: /tmp/cargo/registry/src/index.crates.io-6f17d22bba15001f/zerocopy-0.8.33/build.rs
-
-/tmp/cargo/registry/src/index.crates.io-6f17d22bba15001f/zerocopy-0.8.33/build.rs:
--- src/workspace/rv32i_cpu/simulator/target/release/deps/num_integer-2052bdd82d74f309.d
+++
@@ -1,9 +0,0 @@
-/mnt/d/Tomato_Fish/assassyn/src/workspace/rv32i_cpu/simulator/target/release/deps/libnum_integer-2052bdd82d74f309.rmeta: /tmp/cargo/registry/src/index.crates.io-6f17d22bba15001f/num-integer-0.1.46/src/lib.rs /tmp/cargo/registry/src/index.crates.io-6f17d22bba15001f/num-integer-0.1.46/src/roots.rs /tmp/cargo/registry/src/index.crates.io-6f17d22bba15001f/num-integer-0.1.46/src/average.rs
-
-/mnt/d/Tomato_Fish/assassyn/src/workspace/rv32i_cpu/simulator/target/release/deps/libnum_integer-2052bdd82d74f309.rlib: /tmp/cargo/registry/src/index.crates.io-6f17d22bba15001f/num-integer-0.1.46/src/lib.rs /tmp/cargo/registry/src/index.crates.io-6f17d22bba15001f/num-integer-0.1.46/src/roots.rs /tmp/cargo/registry/src/index.crates.io-6f17d22bba15001f/num-integer-0.1.46/src/average.rs
-
-/mnt/d/Tomato_Fish/assassyn/src/workspace/rv32i_cpu/simulator/target/release/deps/num_integer-2052bdd82d74f309.d: /tmp/cargo/registry/src/index.crates.io-6f17d22bba15001f/num-integer-0.1.46/src/lib.rs /tmp/cargo/registry/src/index.crates.io-6f17d22bba15001f/num-integer-0.1.46/src/roots.rs /tmp/cargo/registry/src/index.crates.io-6f17d22bba15001f/num-integer-0.1.46/src/average.rs
-
-/tmp/cargo/registry/src/index.crates.io-6f17d22bba15001f/num-integer-0.1.46/src/lib.rs:
-/tmp/cargo/registry/src/index.crates.io-6f17d22bba15001f/num-integer-0.1.46/src/roots.rs:
-/tmp/cargo/registry/src/index.crates.io-6f17d22bba15001f/num-integer-0.1.46/src/average.rs:
